9f67e9abdae97a4e5003e6b4d9bd234710d5d085
Rothery275 <szymon96mazur@gmail.com>
2022-02-18 15:24:52 +0100

    first commit

---
+++ src/app/currency-selector/currency-selector.component.ts
@@ -0,0 +1,18 @@
+import { Currency } from "./../model/currency";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-currency-selector",
+  templateUrl: "./currency-selector.component.html",
+  styleUrls: ["./currency-selector.component.css"],
+})
+export class CurrencySelectorComponent {
+  selectedValue: Currency;
+  defaultCurrency: string = "";
+  @Input() currencies: Currency[] = [];
+  @Output() onSelect = new EventEmitter<Currency>();
+
+  selectCurrency(): void {
+    this.onSelect.emit(this.selectedValue);
+  }
+}
